640b5a3b824706947f54bcdd4f10e774ba3ee146
Hanssondevelop <hansson.dev@gmail.com>
2022-12-02 18:33:15 +0300

    Here is my work

---
+++ Lesson1/homework/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Homework?</title>
+</head>
+<body>
+	<h2>HTML part:</h2>
+	<table border="1">
+		<tr>
+			<td>Ячейка №1</td>
+			<td>Ячейка №2</td>
+			<td>Ячейка №3</td>
+			<td>Ячейка №4</td>
+			<td>Ячейка №5</td>
+		</tr>
+		<tr>
+			<td>Ячейка №6</td>
+			<td>Ячейка №7</td>
+			<td>Ячейка №8</td>
+			<td>Ячейка №9</td>
+			<td>Ячейка №10</td>
+		</tr>
+		<tr>
+			<td>Ячейка №11</td>
+			<td>Ячейка №12</td>
+			<td>Ячейка №13</td>
+			<td>Ячейка №14</td>
+			<td>Ячейка №15</td>
+		</tr>
+		<tr>
+			<td>Ячейка №16</td>
+			<td>Ячейка №17</td>
+			<td>Ячейка №18</td>
+			<td>Ячейка №19</td>
+			<td>Ячейка №20</td>
+		</tr>
+		<tr>
+			<td>Ячейка №21</td>
+			<td>Ячейка №22</td>
+			<td>Ячейка №23</td>
+			<td>Ячейка №24</td>
+			<td>Ячейка №25</td>
+		</tr>
+	</table>
+
+	<ol>
+		<li>Алгебра</li>
+		<li>Геометрия</li>
+		<li>Русский язык</li>
+		<li>Литература</li>
+		<li>Информатика</li>
+		<li>Английский язык</li>
+		<li>История</li>
+		<li>Обществознание</li>
+		<li>Физкультура</li>
+		<li>Биология</li>
+		<li>Химия</li>
+		<li>География</li>
+		<li>ОБЖ</li>
+	</ol>
+    <!-- Homework has done! !-->
+    <h2>PHP part:</h2>
+	<p>The result is - <?php $test = 55; if ($test < 10) {echo "<b>:-)</b>";} else {echo "<b>:-|</b>";} ?></p> <!-- PHP code inside HTML !-->
+</body>
+
+<style type="text/css">
+	li{
+		font-style: italic;
+		font-family: serif;
+		font-weight: bold;
+	}
+</style>
+
+
+
+</html>
