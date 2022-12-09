808a69ee9f049d0dc4812250991b1cd49dcc8be3
Hanssondevelop <hansson.dev@gmail.com>
2022-12-09 18:44:50 +0300

    lesson3

---
+++ Lesson3/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP_Lessons</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="/second_page.php">На другую страницу!</a>
+    <p class="q">Classes</p>
+    <p class="qq">Classesses</p>
+</body>
+</html>
---
+++ Lesson3/second_page.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP_Lessons</title>
+</head>
+<body>
+    <p>The second Page!</p>
+    <a href="/index.php">Return!</a>
+    <br>
+    <img width="50%" src="https://www.php.net/images/meta-image.png" alt="">
+    <ul type="circle"> <!--  types: circle, disk, square  !-->
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    <ol type="A"> <!-- I - римские a - lower case A - higher case!-->
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ol>
+    <ol type="a">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ol>
+    <ol type="I">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ol>
+</body>
+</html>
